d8bf56b5935f0376e955b3ff42048f72c65215d6
Virender negi <58510282+viren760@users.noreply.github.com>
2021-07-12 22:21:02 +0530

    newly commit

--- Java/Core JAVA/src/Thread/DemonThread.java
+++ Java/Core JAVA/src/Thread/DemonThread.java
@@ -1,10 +1,30 @@
 package Thread;
 
-public class DemonThread {
 
+
+
+
+public class DemonThread extends Thread{
+
+	public void run() {
+		System.out.println(Thread.currentThread().isDaemon());
+		if(Thread.currentThread().isDaemon()) {
+			System.out.println("Daemon Thread");		
+		}else {
+			System.out.println("Child Thread");
+		}
+	
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+        System.out.println("Main Thread");
+		
+		DemonThread d = new DemonThread();
+		d.setDaemon(true);
+		d.start();
+		
+		
 	}
 
 }
